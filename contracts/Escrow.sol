21203bdc1c0bb4c4a4c090674aa7a5a5d23bdc0a
francisco279 <francisco_aleman09@hotmail.com>
2022-11-29 13:27:57 -0600

    Finalizingg contract

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -20,10 +20,12 @@ contract Escrow
 
     //MAPPINGS
    
-    mapping(uint256 => bool)    public isListed;   //Mapping to know if a token was listed or not 
-    mapping(uint256 => uint256) public purchasePrice; //Mapping for set the purchase price 
-    mapping(uint256 => uint256) public escrowAmount; //Mapping tokenID to escrow amount 
-    mapping(uint256 => address) public buyer; //Mapping to set the buyers of each token 
+    mapping(uint256 => bool)                     public isListed;   //Mapping to know if a token was listed or not 
+    mapping(uint256 => uint256)                  public purchasePrice; //Mapping for set the purchase price 
+    mapping(uint256 => uint256)                  public escrowAmount; //Mapping tokenID to escrow amount 
+    mapping(uint256 => address)                  public buyer; //Mapping to set the buyers of each token 
+    mapping(uint256 => bool)                     public inspectionPassed; // To check if the inspection passed 
+    mapping(uint256 => mapping(address => bool)) public approval; // To approve the sale of a nft  
 
     // MODIFIERS
     
@@ -41,6 +43,13 @@ contract Escrow
         _;
     }
 
+    // Modifier: only the inspector can change the passed status of a NFT (real state)   
+    modifier onlyInspector()
+    {
+        require(msg.sender == inspector, "Only seller can call this method");
+        _;
+    }
+
     //CONSTRUCTOR
 
     constructor (address _nftAddress, address payable _seller, address _inspector, address _lender)
@@ -53,7 +62,7 @@ contract Escrow
 
     //FUNCTIONS
 
-    //Send a token from seller to this contract (payable function)
+    //Send a token from seller to this contract (only seller)
     function list(uint256 _nftID, address _buyer, uint256 _purchasePrice, uint256 _escrowAmount) public payable onlySeller
     {
         IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID); //transfer token
@@ -63,11 +72,74 @@ contract Escrow
         buyer[_nftID]         = _buyer; // set the buyer of the each token on the mapping
     }
 
-    //Buyer deposit earnest
+    //Buyer deposit earnest (only buyer)
     function depositEarnest(uint256 _nftID)public payable onlyBuyer(_nftID)
     {
         //Account deposit should be equal or bigger than token cost 
         require(msg.value >= escrowAmount[_nftID]);
     }
+
+    //Function to change the inspection status of a nft (only inspector)
+    function updateInspectionStatus(uint _nftID, bool _passed) public onlyInspector
+    {
+        inspectionPassed[_nftID] = _passed; 
+    }
+
+    //Function to approve a sale 
+    function approveSale(uint256 _nftID) public view
+    {
+        approval[_nftID][msg.sender] == true;
+    }
+
+    //Finalize Sale
+    // -> Require inspection status (add more items here, like appraisal)
+    // -> Require sale to be authorized
+    // -> Require founds to be correct amount
+    // -> Transfer NFT to buyer
+    // -> Transfer founds to Seller
+    function finalizeSale(uint256 _nftID) public
+    {
+        require(inspectionPassed[_nftID]);
+        require(approval[_nftID][buyer[_nftID]]);
+        require(approval[_nftID][seller]);
+        require(approval[_nftID][lender]);
+        require(address(this).balance >= purchasePrice[_nftID]);
+
+        isListed[_nftID] = false;
+
+        //transfer ether from this smart contract to the seller
+        (bool success, ) = payable(seller).call{value: address(this).balance}("");
+        require(success);
+         //transfer token to buyer 
+        IERC721(nftAddress).transferFrom(address(this), buyer[_nftID], _nftID);
+
+    }
+
+    //If the inspection == false refound, otherwise send ether to seller
+    function cancelSale(uint256 _nftID) public 
+    {
+        if (inspectionPassed[_nftID] == false) 
+        {
+            payable(buyer[_nftID]).transfer(address(this).balance);
+        } 
+        else 
+        {
+            payable(seller).transfer(address(this).balance);
+        }
+    }
+
+    //Receive function (without data) to enable the sending of ethers to this contract 
+    receive() external payable
+    {
+
+    }
+
+    //Get the this smart contract balance
+    function getBalance() public view returns(uint256)
+    {
+        return address(this).balance;
+    }
+
+
 
 }
