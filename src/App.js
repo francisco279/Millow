d5cf0e82768528341da508c4e7b09d5e5139a155
francisco279 <francisco_aleman09@hotmail.com>
2022-12-05 11:04:03 -0600

    Added Navigation Component

--- src/App.js
+++ src/App.js
@@ -15,9 +15,32 @@ import config from './config.json';
 
 function App() {
 
+  //State variables
+  const [account, setAccount] = useState(null);
+
+  //Function to connect to blockchain
+  const loadBlockchain = async() =>
+  {
+    const provider = new ethers.providers.Web3Provider(window.ethereum); // Our web3 provider
+
+    //Set up and update the account every time the metamask account changes
+    window.ethereum.on('accountsChanged', async () =>
+    {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' }); // Get accounts
+      const account  = ethers.utils.getAddress(accounts[0])
+      setAccount(account);
+    })
+  }
+
+  useEffect ( () => 
+  {
+    loadBlockchain()
+  }, [])
+
   return (
     <div>
 
+      <Navigation account={account} setAccount={setAccount}></Navigation>
       <div className='cards__section'>
 
         <h3>Welcome to Millow</h3>
